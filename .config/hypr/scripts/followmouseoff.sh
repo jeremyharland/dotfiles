e3168a49b171a5d17c95ef1fb5cfd120cfc83457
jeremy <jeremy.harland@gmail.com>
2025-06-12 06:34:32 +1000

    remove non mac app config

--- .config/hypr/scripts/followmouseoff.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/follow_mouse = 1/follow_mouse = 0/' ~/.config/hypr/hyprland.conf
--- .config/hypr/scripts/openlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the URL under the cursor (requires some application-specific logic)
-url=$(xclip -o)
-
-# Open the URL with the specified browser profile
-firefox -P Mosh "$url"
