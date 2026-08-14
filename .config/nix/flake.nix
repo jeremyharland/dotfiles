ef7ed7862551aae545ef1fae2824495fbc70a7d8
jeremy <jeremy.harland@gmail.com>
2026-08-14 22:51:08 +1000

    update zsh

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -29,10 +29,11 @@
           pkgs.zoxide
           pkgs._1password-cli
           pkgs.mpv
-          pkgs.neofetch
+          pkgs.fastfetch
           pkgs.speedtest-cli
           pkgs.stow
           pkgs.wget
+          pkgs.eza
         ];
 
       fonts.packages = [
@@ -84,7 +85,75 @@
       nixpkgs.config.allowUnfree = true;
 
       # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
+      programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        enableAutosuggestions = true;
+        enableSyntaxHighlighting = true;
+        enableFzfCompletion = true;
+        enableFzfGit = true;
+        enableFzfHistory = true;
+
+        promptInit = ''
+          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+          [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+        '';
+
+        interactiveShellInit = ''
+          # extra completion definitions (replaces oh-my-zsh's zsh-completions plugin)
+          fpath=(${pkgs.zsh-completions}/share/zsh/site-functions $fpath)
+
+          # listing
+          alias l='eza -lh  --icons=auto'
+          alias ls='eza -1   --icons=auto'
+          alias ll='eza -lha --icons=auto --sort=name --group-directories-first'
+          alias ld='eza -lhD --icons=auto'
+
+          # misc
+          alias vim='nvim'
+          alias pn='pnpm'
+          alias python=python3
+          alias pip=pip3
+          alias mkdir='mkdir -p'
+
+          # navigation
+          alias ..='cd ..'
+          alias ...='cd ../..'
+          alias .3='cd ../../..'
+          alias .4='cd ../../../..'
+          alias .5='cd ../../../../..'
+
+          # git (replaces oh-my-zsh's git plugin)
+          alias gst='git status'
+          alias gss='git status -s'
+          alias ga='git add'
+          alias gaa='git add --all'
+          alias gc='git commit -v'
+          alias gcmsg='git commit -m'
+          alias gco='git checkout'
+          alias gcb='git checkout -b'
+          alias gb='git branch'
+          alias gp='git push'
+          alias gpl='git pull'
+          alias gl='git log --oneline --graph --decorate'
+          alias gd='git diff'
+          alias gds='git diff --staged'
+          alias grh='git reset HEAD'
+          alias gsta='git stash'
+          alias gstp='git stash pop'
+
+          # gh (GitHub CLI)
+          alias ghpr='gh pr create'
+          alias ghprv='gh pr view --web'
+          alias ghprs='gh pr status'
+          alias ghi='gh issue list'
+          alias ghr='gh repo view --web'
+          alias ghc='gh repo clone'
+
+          eval "$(zoxide init zsh)"
+          eval "$(mise activate zsh)"
+        '';
+      };
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
